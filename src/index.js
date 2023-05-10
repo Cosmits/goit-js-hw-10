9005a2d1d237e05daea9dd6801127b03ed7597f7
Hennadii Samofal <salge.cos@gmail.com>
2023-05-11 00:21:43 +0200

    Revert "task01"

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 import './css/styles.css';
 
-//library
-import debounce from 'lodash.debounce';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-//const
-import { refs } from './models/refs';
-import { DEBOUNCE_DELAY } from './models/env';
-//function
-import fetchCountries from './components/fetchCountries';
-import onSearchBox from './components/onSearchBox';
-
-refs.searchBox.addEventListener('input', debounce(onSearchBox, DEBOUNCE_DELAY));
+const DEBOUNCE_DELAY = 300;
--- src/models/refs.js
+++
@@ -1,5 +0,0 @@
-export const refs = {
-  searchBox: document.getElementById('search-box'),
-  countryList: document.querySelector('.country-list'),
-  countryInfo: document.querySelector('.country-info'),
-}
