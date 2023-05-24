a487f63352756243f0aa2cb42ebccbb67499ed16
Cosmits <salge.cos@gmail.com>
2023-05-24 15:33:49 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 //const
 import { refs } from './models/refs';
-//function
-import onSearchBox from './components/onSearchBox';
+import onSearchBox from './components/on-search-box';
+
 
 const DEBOUNCE_DELAY = 300;
 
-refs.searchBox.addEventListener('input', debounce(onSearchBox, DEBOUNCE_DELAY));
+refs.searchBox.addEventListener('input', debounce(onSearchBox , DEBOUNCE_DELAY));
